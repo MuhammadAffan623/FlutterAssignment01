d0b6771b183125c49f6871cb2caa44fc516b8545
bhola-tech <65225257+bhola-tech@users.noreply.github.com>
2021-05-30 17:38:34 +0500

    Add files via upload

--- main.dart
+++ main.dart
@@ -8,8 +8,35 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Text("Muhammad Affan"),
+        appBar: AppBar(
+          title: Center(child: Text("login page")),
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 50,
+              ),
+              Container(
+                width: 200,
+                child: TextField(),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Container(
+                width: 200,
+                child: TextField(),
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              ElevatedButton(onPressed: () {}, child: Text("login"))
+            ],
+          ),
+        ),
       ),
     );
   }
